a9f38cd53df3a7cf25a9f3d7bdb4e914c3dd7815
antunahuel <dgarridoantinao@gmail.com>
2024-03-12 23:14:00 -0300

    se añdio resultado final del juego

--- js/cachipun_script.js
+++ js/cachipun_script.js
@@ -1,53 +1,61 @@
 //DESAFÍO CACHIPÚN
 
 
-let juego=0;
-let score_jugador=0;
-let score_maquina=0;
-let empate=0;
-let resultado="";
+let juego = 0;
+let score_jugador = 0;
+let score_maquina = 0;
+let empate = 0;
+let resultado = "";
 
 
 function main() {
-    let juego= Number(prompt("Cuántas veces deseas jugar"));
+    let juego = Number(prompt("Cuántas veces deseas jugar"));
 
-    for (i=1; i<=juego; i++){
+    for (i = 1; i <= juego; i++) {
         jugar(i);
     }
 
     resultado += "Puntaje jugador = " + score_jugador;
     resultado += "\nPuntaje Maquina = " + score_maquina;
     resultado += "\nEmpates = " + empate;
-1
+
     alert(resultado);
 
-    alert("game over");
-}
-
-function aleatorio(){
-       return Math.floor(Math.random()*3)
+    if (score_jugador > score_maquina) {
+        document.writeln("Haz ganado a la máquina con... ", score_jugador, "ptos.");
+    } else if (score_jugador == score_maquina) {
+        alert("empate");
+    }
+    else {
+        alert("game over...esta vez gano la máquina con:" + "" + score_maquina + "ptos.");
     }
 
+}
+
+function aleatorio() {
+    return Math.floor(Math.random() * 3)
+}
+
 function jugar() {
-    let jugador=prompt("escoge una opcion: 1.- Piedra 2.- Papel 3.- Tijera");
-    maquina=aleatorio();
+    let jugador = prompt("escoge una opcion: 1.- Piedra 2.- Papel 3.- Tijera");
+    maquina = aleatorio();
 
     if (jugador == maquina) {
-            empate++;
-            alert("EMPATE");
-        } else if (jugador == 1 && maquina == 3){
-            score_jugador++;
-            alert("GANASTE");
-        } else if (jugador == 2 && maquina == 1){
-            score_jugador++;
-            alert("GANASTE");
-        } else if (jugador == 3 && maquina == 2){
-            score_jugador++;
-            alert("GANASTE");
-        } else {
-            score_maquina++;
-            alert("PERDITE");
-        }
+        empate++;
+        alert("EMPATE");
+    } else if (jugador == 1 && maquina == 3) {
+        score_jugador++;
+        alert("GANASTE");
+    } else if (jugador == 2 && maquina == 1) {
+        score_jugador++;
+        alert("GANASTE");
+    } else if (jugador == 3 && maquina == 2) {
+        score_jugador++;
+        alert("GANASTE");
+    } else {
+        score_maquina++;
+        alert("PERDISTE");
+    }
 }
 
 main();
